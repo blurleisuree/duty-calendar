65e252355aae57a65391e520ae5a02c814d09455
blurleisure <ipishir@gmail.com>
2025-04-19 13:19:59 +0300

    feat: add base routes

--- client/src/App/data/routes.jsx
+++ client/src/App/data/routes.jsx
@@ -1,14 +1,23 @@
 import { Navigate } from "react-router";
 
-import Inner from "../../pages/Inner/Inner";
+import MainPage from "../../pages/MainPage/MainPage";
+import Calendar from "../../pages/CalendarWrapper/CalendarWrapper";
+import Item from "../../pages/ItemWrapper/ItemWrapper";
+import Inner from '../../pages/Inner/Inner'
 
 export const routeConfig = [
   {
     path: "/",
-    element: <Inner />,
+    element: <MainPage />,
+    children: [
+      { path: "", element: <Navigate to="calendar" replace /> },
+      { path: "calendar", element: <Calendar /> },
+      { path: "calendar/:date", element: <Item /> },
+      { path: "admin", element: <Inner /> },
+    ],
   },
   {
     path: "*",
-    element: <Navigate to="/" replace />,
+    element: <Navigate to="/calendar" replace />,
   },
 ];
