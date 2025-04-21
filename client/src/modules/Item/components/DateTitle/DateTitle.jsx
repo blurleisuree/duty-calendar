f1c061ab4a949b1cc8b1ff01a5e13dc4762b74c7
blurleisure <ipishir@gmail.com>
2025-04-21 23:21:49 +0300

    feat: add arrows to redirect on Datetitle

--- client/src/modules/Item/components/DateTitle/DateTitle.jsx
+++ client/src/modules/Item/components/DateTitle/DateTitle.jsx
@@ -1,5 +1,9 @@
-import { format } from "date-fns";
+import { format, addDays } from "date-fns";
 import { ru } from "date-fns/locale";
+
+import { useNavigate } from "react-router";
+
+import arrow from "../../../../assets/icons/itemArrow.svg";
 
 function DateTitle({ date }) {
   const dayOfWeekName = format(date, "EEEE", { locale: ru });
@@ -10,9 +14,29 @@ function DateTitle({ date }) {
     year: "numeric",
   });
 
+  const navigate = useNavigate();
+  const handleClick = (direction) => {
+    return () => {
+      if (direction === "next") {
+        const nextDate = format(addDays(date, 1), "yyyy-MM-dd")
+        navigate(`/calendar/${nextDate}`);
+      } else if (direction === "prev") {
+        const prevDate = format(addDays(date, -1), "yyyy-MM-dd");
+        navigate(`/calendar/${prevDate}`);
+      }
+    };
+  };
+
   return (
-    <h3 className="text-primary text-lg text-center mt-4 border-b-line border-b pb-4 capitalize">
+    <h3 className="text-primary text-lg text-center mt-4 border-b-line border-b pb-4 capitalize flex items-center justify-between px-5 ">
+      <img src={arrow} alt="arrow" onClick={handleClick("prev")} className="cursor-pointer p-2"/>
       {dayOfWeekName} – {localDate}
+      <img
+        src={arrow}
+        alt="arrow"
+        className="transform rotate-180 cursor-pointer p-2"
+        onClick={handleClick("next")}
+      />
     </h3>
   );
 }
