6faeec07be5c0e9efd3dd9ba1c6b2633f53b998e
blurleisure <ipishir@gmail.com>
2025-04-24 02:08:53 +0300

    feat: add pwa logic, favicons

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,72 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import UnoCSS from "unocss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), UnoCSS()],
+  plugins: [
+    react(),
+    UnoCSS(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: [
+        "favicons/favicon.ico",
+        "favicons/apple-touch-icon.png",
+        "favicons/pwa-192x192.png",
+        "favicons/pwa-512x512.png",
+      ],
+      manifest: {
+        name: "DutyDays",
+        short_name: "DutyDays",
+        description: "Приложение для управления дежурствами",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "/favicons/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/favicons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/favicons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === "document",
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "html-cache",
+            },
+          },
+          {
+            urlPattern: ({ request }) =>
+              ["style", "script", "worker"].includes(request.destination),
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "assets-cache",
+            },
+          },
+        ],
+      },
+    }),
+  ],
 });
+
