0dfb908c2a3713c048453c91c58910984d84cde7
blurleisure <ipishir@gmail.com>
2025-04-24 02:13:11 +0300

    fix: replaced fetch duties from app to mainpage

--- client/src/pages/MainPage/MainPage.jsx
+++ client/src/pages/MainPage/MainPage.jsx
@@ -1,8 +1,23 @@
 import { Outlet } from "react-router";
+import { useEffect } from "react";
+
+import useDutyStore from "../../shared/store/dutyStore.js";
+import { useAuthStore } from "../../modules/Auth/index.js";
 
 import { Header } from "../../modules/Header/index";
 
+import Loader from "../../shared/components/UI/Loader/Loader.jsx";
+
 function MainPage() {
+  const fetchDuties = useDutyStore((state) => state.fetchDuties);
+  const isLoading = useDutyStore((state) => state.isLoading);
+
+  useEffect(() => {
+    fetchDuties();
+  }, [fetchDuties]);
+
+  if (isLoading) return <Loader fullPage={true} />;
+
   return (
     <div>
       <Header />
