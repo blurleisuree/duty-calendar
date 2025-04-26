b509aab766a9a3a42a19935691ccd44db819ae19
blurleisure <ipishir@gmail.com>
2025-04-26 11:52:21 +0300

    fix: fix offline addnewduties

--- client/src/modules/Auth/components/ToggleUser/ToggleUser.jsx
+++ client/src/modules/Auth/components/ToggleUser/ToggleUser.jsx
@@ -1,11 +1,8 @@
-import { useEffect } from "react";
 import useAuthWayStore from "../../store/authWayStore";
 
 function ToggleUser() {
   const { toggleAuthWay, authWayIsAdmin } = useAuthWayStore();
-  useEffect(() => {
-    console.log(authWayIsAdmin);
-  }, [authWayIsAdmin]);
+
   return (
     <p
       className="mt-5 text-center text-active underline-1 underline cursor-pointer"
