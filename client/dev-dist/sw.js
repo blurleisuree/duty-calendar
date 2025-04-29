5d17686763cbe35e6e0c49ce3d918f2573081256
blurleisure <ipishir@gmail.com>
2025-04-29 22:21:41 +0300

    chore: new xlsx files

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ce847fdc'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3vvrhptmmc"
+    "revision": "0.450d79d983o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
