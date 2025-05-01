7db09e0068c49b22f49a73448b8429f35e97a7aa
blurleisure <ipishir@gmail.com>
2025-05-01 15:31:00 +0300

    fix: fix orgselector width

--- client/src/shared/components/OrgSelector/OrgSelector.jsx
+++ client/src/shared/components/OrgSelector/OrgSelector.jsx
@@ -26,7 +26,7 @@ function OrgSelector() {
   }, [getAllOrgs, duties]);
 
   return (
-    <div className="relative h-min mt-5" onClick={toggleDropDown}>
+    <div className="relative h-min mt-5 max-w-screen" onClick={toggleDropDown}>
       <div className="inline-flex items-center w-full justify-center text-xl text-active font-300 cursor-pointer active:opacity-40 transition">
         {activeOrg}
         <img src={arrow} alt="arrow" className="ml-1" />
